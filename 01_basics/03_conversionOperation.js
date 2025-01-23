ec9f3370cc81b0228f64aa189bb95db3805f2d1f
Tejas Sonawane <tejassonawane3109@gmail.com>
2025-01-23 13:13:10 +0530

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -19,3 +19,42 @@ let booleanIsLoggedIn=Boolean(isLoggedIn )
 let someNumber=23
 let stringNumber=String(someNumber)
 console.log(stringNumber)
+
+
+//------Operations------
+
+let value=3
+let nagValue=-value
+console.log(nagValue)
+
+
+let str1="Hello"
+let str2=" Tejas"
+let str3=str1+str2
+console.log(str3)
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+3)// in this case it will first concatnate 1 and 2 and then concatnate 3
+console.log(1+2+"3")//in this case it will first add 1+2 and then concatnate 3
+
+console.log((3+4)*2%6)
+
+console.log(+true)
+console.log(+"")
+
+
+let num1,num2,num3
+num1=num2=num3=2+2
+
+let gameCounter=100
+gameCounter++; //gameCounter=gameCounter+1
+console.log(gameCounter)
+
+let gamePre=100
+++gamePre; //gamePre=1+gamePre
+console.log(gamePre)
+
+
+
+ 
