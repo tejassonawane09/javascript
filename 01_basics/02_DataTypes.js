e29df4f4e7754c3c36e75a28a167104137ac3d18
Tejas Sonawane <tejassonawane3109@gmail.com>
2025-01-23 12:10:35 +0530

    datatypes

---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all javascript as newer version
+
+//  alert(3+3)// we are using nodejs, not browser
+
+console.log(3+3)//code readablity should be high
+console.log("Tejas Sonawane")
+
+
+
+let name="Tejas" //String variable
+let age=22 //number/interger variable
+let isLoggedIn=false //boolean variable
+let state=null //null variable
+
+//number => 2 to power 53
+//BigInt => 2 to power 53 to 2 to power 64
+//string => "Tejas" or 'Tejas'
+//boolean => true or false 
+//null =>null standalone value
+//undefined => undefined standalone value
+//object => {name:"Tejas",age:20}
+//symbol => symbol('name') //unique value
+
+console.log(typeof "Tejas") //typeof is used to check the type of value 
+console.log(typeof undefined)//undefined is a type of value
+console.log(typeof null)//null is a type of object
+
