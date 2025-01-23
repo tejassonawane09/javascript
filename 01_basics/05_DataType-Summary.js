0b596f71811132c37c1b7954ea0aa93cc462cd70
Tejas Sonawane <tejassonawane3109@gmail.com>
2025-01-23 14:22:01 +0530

    datatype summary

---
+++ 01_basics/05_DataType-Summary.js
@@ -0,0 +1,35 @@
+ // Primitive Datatype(call by value)
+      // 7 types: String,Number,Boolean,Null,Undefined,Symbol,BigInt
+
+
+const score=100
+const scoreValue=100.3
+
+const isLoggedIn=false
+const outsideTemp=null
+let userEmail;
+
+const id=Symbol('123')
+const anotherId=Symbol('123')
+
+console.log(id===anotherId);
+
+const bigNumber=43245654n
+
+
+
+
+//Reference Datatype(call by reference)
+  //Array,Objects,functions
+
+const heros=["shakiman","naagraj","doga"]
+let myObj={
+    name:"Tejas",
+    age:20
+}
+
+const myfunction= function (){
+    console.log("Hello")
+}
+
+console.log(typeof bigNumber);
