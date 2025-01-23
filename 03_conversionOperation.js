887a1c9c26ce0034e9f8434bfe310fee3e5259b8
Tejas Sonawane <tejassonawane3109@gmail.com>
2025-01-23 12:36:50 +0530

    conversionOfDatatypes

---
+++ 03_conversionOperation.js
@@ -0,0 +1,21 @@
+let score=23
+console.log(typeof score)//check the type of value
+
+let valueInNumber=Number(score)//convert the value to number
+console.log(typeof valueInNumber)
+
+
+//"23" =>33
+//"23abc" => NaN
+//true => 1; false =>0
+
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn )
+// 1=> true ; 0=>false
+//""=> false
+//"Tejas" =>true
+
+
+let someNumber=23
+let stringNumber=String(someNumber)
+console.log(stringNumber)
