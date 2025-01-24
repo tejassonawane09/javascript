e6346994be5bccc5201a43911a4ae821d522d006
Tejas Sonawane <tejassonawane3109@gmail.com>
2025-01-24 16:29:54 +0530

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -2,20 +2,64 @@ var c=300
 
  let a=300
 
-if(true){//block code
+if(true){//block code //scope
     let a=10
     const b=20
-    console.log("INNER:",a);
+   // console.log("INNER:",a);
     
 }
 
-for (let i = 0; i < array.length; i++) {
-    const element = array[i];
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
     
-}
+// }
 
- console.log(a);
+ //console.log(a);
 // console.log(b);
-console.log(c);
+//console.log(c);
+
+function one(){
+    const username='Tejas'
+
+        function two(){
+            const website='Youtube'
+            console.log(username);
+            
+        }
+       // console.log(website);
+        
+        two()
+}
+//one()
+
+
+if (true) {
+    const username='Tejas'
+    if (username==='Tejas') {
+        const website=' Youtube'
+        console.log(username+website);
+        
+    }
+    //console.log(website);  
+}
+//console.log(username);//check the scope // not execute
+
+
+//-------------------Interresting----------------
+
+function  addOne(num){
+    return num +1//5+1=6
+}
+console.log(addOne(5));
+
+
+
+const addTwo = function(num){
+    return num +2//5+2=7
+}
+console.log(addTwo(5));
+
+
+
 
 
