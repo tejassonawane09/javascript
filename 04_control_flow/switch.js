866d34102bb635195cd54ed516119889cdef53df
Tejas Sonawane <tejassonawane3109@gmail.com>
2025-01-25 18:34:45 +0530

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,57 @@
+
+// syntax:
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+// const month=3
+// switch (month) {
+//     case 1:
+//         console.log("Jan");
+//         break;
+//     case 2:
+//         console.log("Feb");
+//         break;
+//     case 3:
+//         console.log("Mar");
+//         break;
+//     case 4:
+//         console.log("Apr");
+//         break;
+
+//     default:
+//         console.log("Default Case Match");
+        
+//         break;
+// }
+
+
+
+
+const month="mar"
+switch (month) {
+    case "jan":
+        console.log("Jan");
+        break;
+    case "feb":
+        console.log("Feb");
+        break;
+    case "mar":
+        console.log("Mar");
+        break;
+    case "Apr":
+        console.log("Apr");
+        break;
+
+    default:
+        console.log("Default Case Match");
+        
+        break;
+}
