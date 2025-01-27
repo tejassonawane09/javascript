f25b359f076af57228d86f7fad4278065c03b1eb
Tejas Sonawane <tejassonawane3109@gmail.com>
2025-01-27 12:55:24 +0530

    while_&_dowhile_loop

---
+++ 05_interations/two.js
@@ -0,0 +1,26 @@
+//while
+
+let index =0
+while (index<=10) {
+    console.log(`Value:${index}`);
+    index++    
+}
+
+let myArray=['ironman','thor','captain_america']
+
+let arr=0
+while (arr<myArray.length) {
+    console.log(`Length Of Array:${myArray[arr]}`);
+    arr++
+}
+
+
+
+
+
+//do while
+let score=1
+do {
+    console.log(`Score Is:${score}`);
+    score++
+} while (score <= 10);
